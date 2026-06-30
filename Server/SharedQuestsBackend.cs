85e14e3ff0b1fb32cd719267bbc17b0c9cb7e500
Chris Amori <chris.amori93@gmail.com>
2026-06-29 19:39:43 -0700

    Bump version to 2.0.0

--- Server/SharedQuestsBackend.cs
+++ Server/SharedQuestsBackend.cs
@@ -21,7 +21,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "SharedQuests Backend";
     public override string Author { get; init; } = "SharedQuests";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
